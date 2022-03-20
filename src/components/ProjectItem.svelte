63f77f88bee99a3b3933472e7cb802c553c0d904
Tanner Heffner <tannerjheffner@gmail.com>
2022-03-19 23:24:22 -0700

    projectitem styles

--- src/components/ProjectItem.svelte
+++ src/components/ProjectItem.svelte
@@ -6,12 +6,20 @@
 </script>
 
 <a sveltekit:prefetch
-	  class="w-full no-underline !bg-unset"
-    href={'/work/' + item.slug}
+	  class="project block w-full no-underline mb-8 !bg-unset"
+    href={'/work/' + href}
 >
   <div>
-    <p class="font-bold text-lg text-lime-500">{item.data.name}</p>
+    <p class="font-bold text-lg text-indigo-700 dark:text-lime-500">{item.data.name}</p>
 
     <p class="text-slate-800 dark:text-gray-400 mb-2">{item.data.description}</p>
   </div>
 </a>
+
+<style>
+	.project:hover {
+		p {
+			@apply text-orange-600 dark:text-yellow-400;
+		}
+	}
+</style>
