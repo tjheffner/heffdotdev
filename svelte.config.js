f03b84f63bbfe165eac438ff9526574e2c8c488f
Tanner Heffner <tannerjheffner@gmail.com>
2022-11-09 22:55:59 -0800

    use netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { mdsvex } from 'mdsvex';
 import remarkGithub from 'remark-github';
 import remarkAbbr from 'remark-abbr';
