2b21001dfa4f70db92468442d85d58de8f1380f1
Tanner Heffner <tannerjheffner@gmail.com>
2022-11-10 21:10:56 -0800

    update blog post rendering

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,11 +1,17 @@
 export const SITE_URL = 'https://heffner.netlify.app';
 export const GH_USER = 'tjheffner';
 export const GH_USER_REPO = 'tjheffner/heffdotdev'; // used for pulling github issues and offering comments
-export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
 export const SITE_TITLE = 'heffner.dev';
 export const SITE_DESCRIPTION = 'personal site of tanner heffner';
 export const DEFAULT_OG_IMAGE = 'https://avatars.githubusercontent.com/u/11279744';
 export const MY_TWITTER_HANDLE = 'foodpyramids';
+
+export const APPROVED_POSTERS_GH_USERNAME = ['tjheffner'];
+export const GH_PUBLISHED_TAGS = ['Published'];
+
+// auto generated variables
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const REPO_OWNER = GH_USER_REPO.split('/')[0];
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
