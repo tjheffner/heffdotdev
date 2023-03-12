1fa80d18233cbbd6ec6883c1f826660f40a4bc23
Tanner Heffner <tannerjheffner@gmail.com>
2023-03-12 16:26:48 -0700

    fix indents

--- src/components/Og.svelte
+++ src/components/Og.svelte
@@ -9,19 +9,19 @@
 </div>
 
 <style>
- .outer {
-		font-family: 'PT Serif';
-		display: flex;
-		flex-direction: column;
-		font-size: 70px;
-		background-color: white;
-		height: 100%;
-		width: 100%;
-		align-items: center;
-		justify-content: center;
+  .outer {
+    font-family: 'PT Serif';
+    display: flex;
+    flex-direction: column;
+    font-size: 70px;
+    background-color: white;
+    height: 100%;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
     opacity: 0.75;
     background-image: linear-gradient(270deg, #1e293b, #0c4a6e);
-	}
+  }
 
   .inner {
     background-image: linear-gradient(135deg, #FCDB02, #ca8a04);
