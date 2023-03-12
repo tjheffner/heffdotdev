e3b3582680340ae4fcbad512fddc2268e4285de4
Tanner Heffner <tannerjheffner@gmail.com>
2023-03-12 15:10:59 -0700

    hook endpoint up to card with dynamic message

--- src/routes/og/+server.js
+++ src/routes/og/+server.js
@@ -1,44 +1,42 @@
-// see https://geoffrich.net/posts/svelte-social-image/
 import satori from 'satori';
-import {Resvg} from '@resvg/resvg-js';
+import { Resvg } from '@resvg/resvg-js';
 import PTSerif from '$lib/font/PTSerif-Regular.ttf';
+import { html as toReactNode } from 'satori-html';
+import Og from '../../components/Og.svelte';
 
 const height = 630;
 const width = 1200;
 
 /** @type {import('./$types').RequestHandler} */
-export const GET = async () => {
-  const html = {
-    type: 'div',
-    props: {
-      children: 'hello, world',
-      style: {color: 'red'}
-    }
-  };
-  const svg = await satori(html, {
-    fonts: [
-      {
-        name: 'PT Serif',
-        data: Buffer.from(PTSerif),
-        style: 'normal'
-      }
-    ],
-    height,
-    width
-  });
+export const GET = async ({ url }) => {
+	const message = url.searchParams.get('message') ?? undefined;
+	const result = Og.render({ message });
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
 
-  const resvg = new Resvg(svg, {
-    fitTo: {
-      mode: 'width',
-      value: width
-    }
-  });
+	const svg = await satori(element, {
+		fonts: [
+			{
+				name: 'PT Serif',
+				data: Buffer.from(PTSerif),
+				style: 'normal'
+			}
+		],
+		height,
+		width
+	});
 
-  const image = resvg.render();
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: width
+		}
+	});
 
-  return new Response(image.asPng(), {
-    headers: {
-      'content-type': 'image/png'
-    }
-  });
+	const image = resvg.render();
+
+	return new Response(image.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
 };
