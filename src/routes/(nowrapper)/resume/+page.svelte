e763f894fe896a5a446f27f4bffedc1586adc125
Tanner Heffner <tannerjheffner@gmail.com>
2025-04-02 22:11:08 -0700

    resume updates

--- src/routes/(nowrapper)/resume/+page.svelte
+++ src/routes/(nowrapper)/resume/+page.svelte
@@ -11,7 +11,7 @@
 <body>
   <header>
     <h1>Tanner Heffner</h1>
-    <p>I am a full-stack typescript engineer with a record of delivering successful projects at scale for organizations in a variety of industries. I am a big advocate for accessible design systems and responsive component libraries. Let's build something cool together.</p>
+    <p>I am a full-stack engineer with a record of delivering successful projects at scale for organizations in a variety of industries. I am a big advocate for accessible design systems and responsive component libraries. Let's build something cool together.</p>
   </header>
   <section>
     <h2>Contact</h2>
@@ -34,14 +34,17 @@
       <li>Design Systems</li>
       <li>Accessibility</li>
       <li>UX</li>
+      <li>DevOps</li>
       <li>Node</li>
       <li>HTML</li>
       <li>CSS</li>
       <li>React</li>
       <li>PHP</li>
       <li>Drupal</li>
+      <li>AWS</li>
       <li>Python</li>
       <li>Ruby</li>
+      <li>Public Speaking</li>
       <li>Cypress</li>
       <li>Playwright</li>
       <li>Storybook</li>
@@ -56,15 +59,15 @@
         <p>October 2021 — present</p>
         <ul>
           <li><strong>Architected the modern headless foundation</strong> for <a href="https://va.gov">VA.gov</a>, reducing publishing times for thousands of editors from hours down to sub five minutes.</li>
-          <li>Assisted with <strong>benefits claims processing improvements across the VA stack</strong> as part of the PACT Act. Including but not limited to: <strong>accessibility updates</strong> to custom React form inputs, improving <strong>OCR processing</strong> of PDF claim data in Python, and adding new features for Accredited Representatives helping Veterans with the claims process.</li>
-          <li>Rapidly <strong>prototyped technical solutions</strong> leading to SWIFT and SPRUCE contract vehicle wins, <strong>receiving highest technical marks</strong> from government evaluators</li>
+          <li>Assisted with <strong>benefits claims processing improvements across the VA stack</strong> as part of the PACT Act. Including but not limited to: <strong>accessibility updates</strong> to custom React form inputs, improving <strong>OCR processing</strong> of PDF claim data in Python, and <strong>adding new features in React and Ruby</strong> for Accredited Representatives helping Veterans with the claims process.</li>
+          <li>Quick <strong>prototyping of technical solutions</strong> leading to SWIFT and SPRUCE contract vehicle wins, <strong>receiving highest technical marks</strong> from government evaluators.</li>
         </ul>
 
         <h3 id="Coforma">Coforma</h3>
         <h4 id="senior dev 2">Senior Developer</h4>
         <p>January 2023 — June 2023</p>
         <ul>
-          <li>For the No Surprises Act, architected the <a href="https://www.cms.gov/medical-bill-rights">Medical Bill Rights</a> sub-site for CMS.gov with localized content in English and Spanish.</li>
+          <li>For the No Surprises Act, <strong>architected the <a href="https://www.cms.gov/medical-bill-rights">Medical Bill Rights</a> sub-site</strong> for CMS.gov with localized content in English and Spanish.</li>
           <li><strong>Led front-end implementation</strong>, providing direction to junior team members and actively building out the React form architecture including <strong>localization, field and schema validations, plus Salesforce and Drupal integration.</strong>.</li>
         </ul>
 
@@ -73,7 +76,7 @@
         <p>October 2019 — March 2021</p>
         <ul>
           <li>Developed a bespoke <strong>responsive component library</strong> combined with an <strong>A/B testing system</strong> with Craft CMS, allowing site builders to continously optimize with confidence.</li>
-          <li>Rapidly <strong>prototyped brand explorations</strong> with React and Storybook for the former Twitter.com design team</li>
+          <li>Rapidly <strong>prototyped brand explorations</strong> with React and Storybook for the former Twitter.com design team.</li>
           <li>Created <strong>custom responsive components</strong> for <a href="https://google.org">Google.org</a> using Python and vanilla Javascript.</li>
           <li><strong>Built new React components</strong> for <a href="https://business.pinterest.com/getting-started/">Pinterest Business</a>.</li>
           <li>Helped <strong>implement the achievements functionality for games and social sharing</strong> across the Epic Games platform.</li>
@@ -85,7 +88,7 @@
         <ul>
           <li><strong>Co-maintained the agency front-end tooling</strong>, enabling the rapid stand-up of new sites with atomic design principles & consistent processes.</li>
           <li>Helped <strong>multiple enterprise organizations</strong> migrate from Drupal 7 to Drupal 8 efficiently.</li>
-          <li><strong>Won a Webby award</strong> for the revamped <a href="https://arsenal.com">Arsenal.com</a></li>
+          <li><strong>Won a Webby award</strong> for the revamped <a href="https://arsenal.com">Arsenal.com</a>.</li>
         </ul>
 
       <h2 id="recent-projects">Recent Projects</h2>
@@ -100,7 +103,7 @@
         <p><a href="https://www.cms.gov/medical-bill-rights">cms.gov/medical-bill-rights</a></p>
 
         <h3 id="greenhouse">Greenhouse</h3>
-        <p>Bespoke marketing website for a leading hiring solutions company. Using Craft CMS, Twig and vanilla JavaScript, I built most of the components, the optimizely a/b integration, as well as the accessibility handling throughout. (keyboard and focus handling, lazy loading images, etc.)</p>
+        <p>Bespoke marketing website for a leading hiring solutions company. Using Craft CMS, Twig and vanilla JavaScript, I built most of the components, the A/B testing integration, as well as the accessibility handling sitewide for things like keyboard and focus handling, lazy loading images, etc.</p>
         <p><a href="https://greenhouse.com">greenhouse.com</a></p>
      </div>
    </main>
