1e1a6699bb2f68da03c5783a324c53adb06b4095
Tanner Heffner <tannerjheffner@gmail.com>
2025-04-03 18:31:37 -0700

    clean up components

--- src/lib/components/Toc.svelte
+++ src/lib/components/Toc.svelte
@@ -48,7 +48,7 @@
 <style>
   .toc-list {
     @apply text-secondary py-5 px-8 -mt-10 space-y-1 overflow-auto transition-all duration-500;
-    @apply bg-slate-900;
+		@apply bg-background;
   }
   .toc-active {
     @apply text-accent font-bold border-s-2 border-current;
