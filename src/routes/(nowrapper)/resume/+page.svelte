75b07186a06011ce5cb7b310b3691d689ae23d4f
tanner heffner <tannerjheffner@gmail.com>
2025-07-04 17:54:51 -0700

    update resume fonts to match site

--- src/routes/(nowrapper)/resume/+page.svelte
+++ src/routes/(nowrapper)/resume/+page.svelte
@@ -3,9 +3,6 @@
 </script>
 
 <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400..700&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <title>Tanner Heffner's Resume</title>
 </head>
 
